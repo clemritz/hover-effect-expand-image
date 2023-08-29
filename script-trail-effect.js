9181f212e9420f102b6a8915d373bdc196ae5411
Clément Ritz <clement.ritz@orange.fr>
2023-08-29 16:46:24 +0200

    update

--- script-trail-effect.js
+++ script-trail-effect.js
@@ -72,6 +72,56 @@ class ImageTrail {
       mousePos.y,
       0.1
     );
+
+    if(distance > this.threshold) {
+      this.showNextImage();
+
+      ++this,this.zIndexVal;
+      this.imgPosition =
+        this.imgPosition < this.imagesTotal - 1 ? this.imgPosition + 1 : 0;
+      lastMousePos = mousePos;
+    }
+
+    let isIdle = true;
+    for (let img of this.images) {
+      if (img.isActive()) {
+        isIdle = false;
+        break;
+      }
+    }
+
+    if (isIdle && this.zIndexVal !== 1) {
+      this.zIndexVal = 1;
+    }
+
+    requestAnimationFrame(() => this.render());
+  }
+
+  showNextImage() {
+    const img = this.images[this.imgPosition];
+    TweenMax.killTweenOf(img.DOM.el);
+
+    new TimelineMax().set(
+      img.DOM.el, {
+        startAt: {opacity: 0, scale: 1},
+        opacity: 1,
+        scale: 1,
+        zIndex: this.zIndexVal,
+        x: cacheMousePos.x - img.rect.width / 2,
+        y: cacheMousePos.y - img.rect.height / 2
+      },
+      0
+    )
+    .to(
+      img.DOM.el,
+      0.9,
+      {
+        ease: Expo.easeOut,
+        x: mousePos.x - img.rect.width / 2,
+        y: mousePos.y - img.rect.height / 2.
+      },
+      0
+    )
   }
 }
 
