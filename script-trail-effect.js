c90ea3b3fbb9b6c7726a7e6736601b7b45d934c5
Clément Ritz <clement.ritz@orange.fr>
2023-08-29 16:31:13 +0200

    js part updating

--- script-trail-effect.js
+++ script-trail-effect.js
@@ -21,10 +21,58 @@ const getMousePos = (ev) => {
 
 let mousePos = (lastMousePos = cacheMousePos = { x: 0, y: 0 });
 
-window.addEventListener;
+window.addEventListener("mousemove", (ev) => (mousePos = getMousePos(ev)));
 
-class Image {}
+const getMouseDistance = () =>
+  MathUtils.distance(mousePos.x, mousePos.y, lastMousePos.x, lastMousePos.y);
 
-class ImageTrail {}
+class Image {
+  constructor(el) {
+    this.DOM = { el: el };
+    this.defaultStyles = {
+      scale: 1,
+      x: 0,
+      y: 0,
+      opacity: 0,
+    };
+    this.getRect();
+  }
+
+  getRect() {
+    this.rect = this.DOM.el.getBoundingClientRect();
+  }
+  isActive() {
+    return TweenMax.isTweening(this.DOM.el) || this.DOM.el.style.opacity != 0;
+  }
+}
+
+class ImageTrail {
+  constructor() {
+    this.DOM = { content: document.querySelector(".content") };
+    this.images = [];
+    [...this.DOM.content.querySelectorAll("img")].forEach((img) =>
+      this.images.push(new Image(img))
+    );
+    this.imagesTotal = this.images.length;
+    this.imgPosition = 0;
+    this.zIndexVal = 1;
+    this.threshold = 100;
+    requestAnimationFrame(() => this.render());
+  }
+
+  render() {
+    let distance = getMouseDistance();
+    cacheMousePos.x = MathUtils.lerp(
+      cacheMousePos.x || mousePos.x,
+      mousePos.x,
+      0.1
+    );
+    cacheMousePos.y = MathUtils.lerp (
+      cacheMousePos.y || mousePos.y,
+      mousePos.y,
+      0.1
+    );
+  }
+}
 
 const preloadImages = () => {};
